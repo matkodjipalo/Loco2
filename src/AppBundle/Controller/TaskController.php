6559ac5b3a2dcdd1e85dfd5192925dff5059a357
Matko Djipalo <matkodjipalo@gmail.com>
2016-08-03 15:17:25 +0200

    - ajax brisanje taska

--- src/AppBundle/Controller/TaskController.php
+++ src/AppBundle/Controller/TaskController.php
@@ -31,4 +31,30 @@ class TaskController extends Controller
             'tasks' => $tasks,
         ]);
     }
+
+    /**
+     * @Route("/{toDoListId}/tasks/{id}/delete", name="delete_task_from_todo-list")
+     */
+    public function deleteAction($id, $toDoListId, Request $request)
+    {
+        if (!$request->isXMLHttpRequest()) {
+            throw new \Exception("Error Processing Request", 1);
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('AppBundle:Task');
+        $task = $repo->findOneById($id);
+
+        if (!$task) {
+            throw $this->createNotFoundException();
+        }
+
+        $em->remove($task);
+        $em->flush();
+
+        return $this->render('task/ajax_list.html.twig', [
+            'tasks' => $repo->findBy(['toDoList' => $toDoListId]),
+            'search' => ''
+        ]);
+    }
 }
